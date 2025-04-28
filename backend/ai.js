4381aab08ac178bb4e6e8087649fcc9660b001eb
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-04-28 18:11:07 +0530

    Closing this Branch and moving to a repo

--- backend/ai.js
+++ backend/ai.js
@@ -1,4 +1,4 @@
 import { genkit } from "genkit";
-import { googleAI, gemini20Flash} from "@genkit-ai/googleai";
+import { googleAI, gemini20Flash } from "@genkit-ai/googleai";
 
-const ai = genkit({ plugins: [googleAI()], model: gemini20Flash });
+export const ai = genkit({ plugins: [googleAI()], model: gemini20Flash });
